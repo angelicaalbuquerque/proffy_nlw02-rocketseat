e9df2dde0c1ae8b571c8793b3d3e76d296aa9d69
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-12 22:32:06 -0300

    Criação do componente Landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
+import Landing from "./pages/Landing";
+
 import "./assets/styles/global.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <button type="button">Clique aqui</button>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
