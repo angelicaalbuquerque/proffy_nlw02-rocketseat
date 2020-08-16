aab879bd25afb8860763d574dc87325429d3370d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-16 00:01:59 -0300

    Componentização do Header

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,26 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import logoImg from "../../assets/images/logo.svg";
-import backIcon from "../../assets/images/icons/back.svg";
+import PageHeader from "../../components/PageHeader";
 
 import "./styles.css";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Volta" />
-          </Link>
-          <img src={logoImg} alt="Proffy logo" />
-        </div>
-
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
-        </div>
-      </header>
+      <PageHeader />
     </div>
   );
 }
