d98a74046e2db729d5ad6862f78ce6cba74eeec7
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-15 23:29:56 -0300

    Melhorias nas rotas

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logoImg from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
@@ -25,15 +26,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Lecionar" />
             Lecionar
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
