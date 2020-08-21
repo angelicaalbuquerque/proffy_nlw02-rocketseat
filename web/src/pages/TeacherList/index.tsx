dcb5de0f9c2b3a32d61f154ce3ede707d91410f4
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-20 23:30:36 -0300

    Finalização de layout TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
 import "./styles.css";
 
 function TeacherList() {
@@ -25,6 +27,41 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars2.githubusercontent.com/u/52754546?s=460&u=beb81a6de4cfbea7677783e3ab2527e30582478d&v=4"
+              alt="Victor Emidio"
+            />
+            <div>
+              <strong>Vitor Emidio</strong>
+              <span>Introdução à Programação</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias para desenvolvimento web.
+            <br />
+            <br />
+            Apaixonado por aprender e ensinar, transformar wireframes em
+            realidade e fazer com que meus alunos avancem para o próximo nível
+            profissional.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora:
+              <strong>R$50,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
