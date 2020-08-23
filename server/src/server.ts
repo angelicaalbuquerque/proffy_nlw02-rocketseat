97610e2136b6cdf5444d1240cb579f4b9f38720e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-23 00:12:32 -0300

    Criação da rota users

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+  const users = [
+    {
+      name: "Diego",
+      age: 25,
+    },
+    {
+      name: "Emidio",
+      age: 22,
+    },
+  ];
+
+  return response.json(users);
+});
+
+//localhost:3333
+
+app.listen(3333);
