085d6b45a51f60b9e9660d872e6f0ea11860d94e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-23 13:21:19 -0300

    Funcionalidades do server

---
+++ server/README.md
@@ -0,0 +1,12 @@
+# Funcionalidades do Back-End
+
+## Conexões
+
+- Rota para listar o total de conexões realizadas;
+- Rota para criar uma nova conexão (para dizer que existiu uma conexão entre aluno e professor).
+
+## Aulas
+
+- Rota para criar uma aula;
+- Rota para listar aulas;
+  - Filtrar por matéria, dia da semana e horário.
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,21 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  const users = [
-    {
-      name: "Diego",
-      age: 25,
-    },
-    {
-      name: "Emidio",
-      age: 22,
-    },
-  ];
+app.use(express.json());
 
-  return response.json(users);
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello, World" });
 });
-
-//localhost:3333
 
 app.listen(3333);
