98989629ce1ef43c5c6d753ddc1c1fd6d5f36367
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-23 22:30:37 -0300

    classSchedule, classes e users cadastrados no db

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,45 @@
 import express from "express";
 import db from "./database/connection";
+import converHourToMinutes from "./utils/convertHourToMinutes";
 
 const routes = express.Router();
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
 
 routes.post("/classes", async (request, response) => {
   const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
-  await db("users").insert({
+  const insertedUsersIds = await db("users").insert({
     name,
     avatar,
     whatsapp,
     bio,
   });
+
+  const user_id = insertedUsersIds[0];
+
+  const insertedClassesIds = await db("classes").insert({
+    subject,
+    cost,
+    user_id,
+  });
+
+  const class_id = insertedClassesIds[0];
+
+  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+    return {
+      class_id,
+      week_day: scheduleItem.week_day,
+      from: converHourToMinutes(scheduleItem.from),
+      to: converHourToMinutes(scheduleItem.to),
+    };
+  });
+
+  await db("class_schedule").insert(classSchedule);
 
   return response.send();
 });
