06b109190523837211461a9ed5ca847a76fceda3
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-23 22:11:51 -0300

    Criação de usuário

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import db from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello, World" });
+routes.post("/classes", async (request, response) => {
+  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
+
+  await db("users").insert({
+    name,
+    avatar,
+    whatsapp,
+    bio,
+  });
+
+  return response.send();
 });
 
 export default routes;
