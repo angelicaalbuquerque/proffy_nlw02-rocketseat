988220465a3278d700a2a3a2bf79407af2cef28a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-24 22:30:32 -0300

    Verificação da matéria

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,6 +10,26 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.subject as string;
+    const time = filters.time as string;
+
+    if (!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: "Missing filters to search classes",
+      });
+    }
+
+    const timeinMinutes = converHourToMinutes(time);
+
+    const classes = await db("classes").where("classes.subject", "=", subject);
+
+    return response.json(classes);
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,7 @@ const routes = express.Router();
 
 const classesControllers = new ClassesController();
 
+routes.get("/classes", classesControllers.index);
 routes.post("/classes", classesControllers.create);
 
 export default routes;
