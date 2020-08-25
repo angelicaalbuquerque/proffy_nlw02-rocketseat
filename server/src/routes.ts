786f5e7831c5516b65379532c39b0dba3c1b8fe1
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-08-24 23:23:01 -0300

    Listagem de todas as conexões salvas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -11,6 +11,7 @@ const connectionsController = new ConnectionsController();
 routes.get("/classes", classesControllers.index);
 routes.post("/classes", classesControllers.create);
 
+routes.get("/connections", connectionsController.index);
 routes.post("/connections", connectionsController.create);
 
 export default routes;
