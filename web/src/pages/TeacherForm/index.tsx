6a0caa17dc49ee908bc2158861238419ed175574
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-09-05 23:31:35 -0300

    Alteração de inputs em TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -18,20 +18,9 @@ function TeacherForm() {
         <fieldset>
           <legend>Seus dados</legend>
 
-          <div className="input-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" id="name" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" id="whatsapp" />
-          </div>
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
         </fieldset>
       </main>
     </div>
