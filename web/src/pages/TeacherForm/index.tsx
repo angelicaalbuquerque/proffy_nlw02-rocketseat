2fecbe78cffca290b4c10effef3f201ccc9c596f
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-09-05 22:48:17 -0300

    Subtítulo TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,7 +5,10 @@ import PageHeader from "../../components/PageHeader";
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher este formulário de inscrição."
+      />
     </div>
   );
 }
