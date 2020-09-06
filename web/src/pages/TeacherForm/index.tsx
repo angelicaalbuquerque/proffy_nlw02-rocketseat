5f90bd14ab2d6cc24bb0a3b22a1e24124b381ba9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-09-06 14:08:16 -0300

    Criação de componente Select com options

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,6 +4,7 @@ import PageHeader from "../../components/PageHeader";
 
 import Input from "../../components/Input";
 import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
 
 import warningIcon from "../../assets/images/icons/warning.svg";
 
@@ -30,7 +31,22 @@ function TeacherForm() {
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
+            ]}
+          />
           <Input name="cost" label="Custo da hora por aula" />
         </fieldset>
 
