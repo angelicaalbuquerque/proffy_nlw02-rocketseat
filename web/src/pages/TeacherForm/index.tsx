0aa391bf25f7c0fd6e05697e1e88f3ca719c4ccc
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-09-06 13:55:33 -0300

    Criação de componente Textarea

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 
 import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
 
 import warningIcon from "../../assets/images/icons/warning.svg";
 
@@ -23,6 +24,7 @@ function TeacherForm() {
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
