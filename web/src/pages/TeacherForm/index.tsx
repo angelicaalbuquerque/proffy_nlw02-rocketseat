81c0366e209636fb1a70efe0e0113c9173b5905a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-09-05 23:30:18 -0300

    Criação e estilização de TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
+import Input from "../../components/Input";
+
+import "./styles.css";
+
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
@@ -9,6 +13,27 @@ function TeacherForm() {
         title="Que incrível que você quer dar aulas."
         description="O primeiro passo é preencher este formulário de inscrição."
       />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Nome completo</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
