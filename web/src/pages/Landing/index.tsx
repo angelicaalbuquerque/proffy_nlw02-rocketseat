ef97d2bc192bf48731358121585bf12693f5b8af
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-09-06 21:42:43 -0300

    Exibindo total de conexões

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import { Link } from "react-router-dom";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
-
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
@@ -11,6 +13,16 @@ import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import "./styles.css";
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -38,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas{" "}
+          Total de {totalConnections} conexões já realizadas{" "}
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
