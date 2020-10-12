7aa1c6822cfbc8079b6339515211776473042a7a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-10-12 12:19:42 -0300

    Estruturação da Landing Page

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -1,5 +1,29 @@
 import { StyleSheet } from 'react-native';
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 40,
+    backgroundColor: '#8257E5'
+  },
+
+  banner: {
+    width:'100%',
+    resizeMode: 'contain'
+  },
+
+  title: {
+    fontFamily: 'Poppins_400Regular',
+    color:'#FFF',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+
+  titleBold: {
+    fontFamily: 'Poppins_600SemiBold',
+  }
 
 });
 
