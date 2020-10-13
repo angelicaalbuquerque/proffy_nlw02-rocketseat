bd11f4eab9bcdf1bbf42655b404ba29dca5f44cf
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-10-12 21:23:34 -0300

    Proffys disponíveis mock

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
 
@@ -8,6 +9,12 @@ function TeacherList() {
   return ( 
   <View style={styles.container}>
     <PageHeader title="Proffys disponíveis"/>
+
+    <ScrollView>
+      <TeacherItem/>
+      <TeacherItem/>
+      <TeacherItem/>
+    </ScrollView>
   </View>
   );
 }
