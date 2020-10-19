bb353904defe3171ae2e57065b14917c8ca557de
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-10-18 22:11:10 -0300

    Atualização da lista de conexões

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -29,6 +30,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
 
   function handleLinkToWhatsApp() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
   }
 
